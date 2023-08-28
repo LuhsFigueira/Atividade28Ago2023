c1f694043265d17c4b09f398602586ea0f1f837f
LuhsFigueira <lu-luishenrique@hotmail.com>
2023-08-28 16:32:56 -0300

    inclusão nova atividade

---
+++ atividade/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora IMC</title>
+</head>
+<body>
+<h1>Calculadora IMC</h1>
+    <form action="" method="get">
+        <label>Nome:</label>
+        <input type="text" id="iNome">
+        <br>
+        <br>
+        <label>Idade: </label>
+        <input type="number" id="iIdade">
+        <br>
+        <br>
+        <label>Altura: </label>
+        <input type="text" id="iAltura">
+        <br>
+        <br>
+        <label>Peso: </label>
+        <input type="text" id="iPeso">
+        <br>
+        <br>
+        <button id="bjogar" name="jogar" value="jogar" type="submit">
+            Calcular
+        </button>
+    </form>
+</body>
+</html>
